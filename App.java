7a59e28996b211c99fc5f5534118430837da9314
Ruparajesh <60231638+Ruparajesh@users.noreply.github.com>
2020-02-25 23:45:09 +0530

    Add files via upload

---
+++ App.java
@@ -0,0 +1,13 @@
+package com.Task3.Logger_Task4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
