de53bf25373249925fe961dcd8174fe4f3fde662
Ruparajesh <60231638+Ruparajesh@users.noreply.github.com>
2020-02-26 20:27:20 +0530

    Update Client.java

--- Client.java
+++ Client.java
@@ -13,23 +13,23 @@ public class Client {
 			// TODO Auto-generated method stub
 			Scanner scan =new Scanner(System.in);
 			Demo Interest = new Demo(4000.0,2.5,10.0);
-			LOGGER.error("Simple Interest:".getBytes());
-			LOGGER.error("%.2f\n",Interest.simpleInterest());
-			LOGGER.error("Compound Interest:".getBytes());
-			LOGGER.error("%.2f\n",Interest.compoundInterest());
-			LOGGER.error("Press 1 for STANDARD MATERIAL\nPress 2 for ABOVE STANDARD MATERIAL\nPress 3 for HIGH STANDARD MATERIAL\n".getBytes());
+			LOGGER.error("Simple Interest:");
+			LOGGER.error(Interest.simpleInterest());
+			LOGGER.error("Compound Interest:");
+			LOGGER.error(Interest.compoundInterest());
+			LOGGER.error("Press 1 for STANDARD MATERIAL\nPress 2 for ABOVE STANDARD MATERIAL\nPress 3 for HIGH STANDARD MATERIAL\n");
 			int materialType = scan.nextInt();
 			boolean automatedHome;
 			if(materialType==3) {
-				LOGGER.error("If you want AUTOMATED HOME Enter true otherwise false\n".getBytes());
+				LOGGER.error("If you want AUTOMATED HOME Enter true otherwise false\n");
 				automatedHome=scan.nextBoolean();
 			}else {
 				automatedHome = false;
 			}
-			LOGGER.error("Enter the total area of the house\n".getBytes());
+			LOGGER.error("Enter the total area of the house\n");
 			double totalArea=scan.nextDouble();
 			HouseContructionCost houseCost = new HouseContructionCost(materialType,automatedHome,totalArea);
-			LOGGER.error("%.2f\n",houseCost.totalCostOfContruction());
+			LOGGER.error(houseCost.totalCostOfContruction());
 			scan.close();
 
 		}
